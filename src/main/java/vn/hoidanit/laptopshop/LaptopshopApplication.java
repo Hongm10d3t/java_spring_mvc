f1991631b3ffe8768305f8bed8c95b0e5854c5f6
Hongm10d3t <vudinhhong69@gmail.com>
2026-01-07 15:08:25 +0700

    hehe

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,11 +3,12 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+// exclude: loại bỏ => tức là ở đây cấu hình như ban đầu nhưng loại bỏ thành phân mình mong muốn
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
 	}
-
 }
